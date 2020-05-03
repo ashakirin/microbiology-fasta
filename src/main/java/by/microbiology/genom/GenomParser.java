ced29989aa22a9d029d435bd1e69c477f3585145
sasch <sasch@LAPTOP-RU5CK2ND.fritz.box>
2020-05-03 13:15:29 +0200

    Building a CSV-file with the needed information

--- src/main/java/by/microbiology/genom/GenomParser.java
+++ src/main/java/by/microbiology/genom/GenomParser.java
@@ -1,30 +1,96 @@
 package by.microbiology.genom;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.io.*;
+import java.util.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 
 public class GenomParser {
-    private static Logger LOGGER = LoggerFactory.getLogger(GenomParser.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(GenomParser.class);
+	private static String location = "";
+	private static String protein = "";
+	private static StringBuilder sequence = new StringBuilder();
+	private static String locusTag = "";
+	private static String line;
 
-    public static void main(String[] args) {
-        try {
-            Document doc = Jsoup.connect("https://www.ncbi.nlm.nih.gov/portal/loader/pload.cgi?curl=http%3A%2F%2Fwww.ncbi.nlm.nih.gov%2Fsviewer%2Fviewer.cgi%3Fid%3D448814763%26db%3Dnuccore%26report%3Dfasta%26retmode%3Dtext%26withmarkup%3Don%26tool%3Dportal%26log%24%3Dseqview%26from%3D4352609%26to%3D4352896&pid=0").get();
-            LOGGER.info(doc.title());
-            Elements newsHeadlines = doc.select("#mp-itn b a");
-            LOGGER.info(doc.html());
-            for (Element headline : newsHeadlines) {
-                LOGGER.info("%s\n\t%s",
-                        headline.attr("title"), headline.absUrl("href"));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void writeCSV() throws Exception {
+		FileWriter fileWriter = new FileWriter("microbiology.csv", true);
+		try (PrintWriter writer = new PrintWriter(fileWriter)) {
 
+			StringBuilder sb = new StringBuilder();
+			sb.append(sequence);
+			sb.append(';');
+			sb.append(locusTag + "  PROTEIN: " + protein + "  LOCATION: " + location);
+			sb.append('\n');
+
+			writer.write(sb.toString());
+		}
+	}
+
+	public static void writeCSVHead() throws Exception {
+		FileWriter fileWriter = new FileWriter("microbiology.csv", true);
+		try (PrintWriter writer = new PrintWriter(fileWriter)) {
+
+			StringBuilder sb = new StringBuilder();
+			sb.append("sequence");
+			sb.append(';');
+			sb.append("locus_tag");
+			sb.append('\n');
+
+			writer.write(sb.toString());
+		}
+	}
+
+	public static void setSequence(String line) throws Exception {
+		sequence.append(line);
+	}
+
+	public static void setLocusTag(String line) {
+		if (line.contains("locus_tag")) {
+			locusTag = line.substring(line.indexOf("locus_tag=") + 10);
+			locusTag = locusTag.substring(0, locusTag.indexOf("]"));
+		}
+	}
+
+	public static void setProtein(String line) {
+		protein = line.substring(line.indexOf("protein=") + 8);
+		protein = protein.substring(0, protein.indexOf("] "));
+	}
+
+	public static void setLocation(String line) {
+		location = line.substring(line.indexOf("location=") + 9);
+		location = location.substring(0, location.indexOf("] "));
+	}
+
+	public static void main(String[] args) throws Exception {
+		boolean first = true;
+		writeCSVHead();
+		Scanner pathScanner = new Scanner(System.in);
+		System.out.print("Please insert the fna path: ");
+		String path = pathScanner.next();
+
+		try (Scanner sc = new Scanner(new File(path))) {
+			while (sc.hasNextLine()) {
+				line = sc.nextLine();
+				if (line.charAt(0) != '>') {
+					setSequence(line);
+				} else {
+					if (!first) {
+						writeCSV();
+					}
+					first = false;
+					sequence = new StringBuilder();
+					setLocusTag(line);
+					setProtein(line);
+					setLocation(line);
+				}
+			}
+			setSequence(line);
+			writeCSV();
+		}
+		pathScanner.close();
+		System.out.println("Done!");
+	}
 }
