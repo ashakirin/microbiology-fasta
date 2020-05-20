c3255d9d740fa79becacb50ab4ae4cded7a6ccef
sasch <sasch@LAPTOP-RU5CK2ND.fritz.box>
2020-05-20 17:55:06 +0200

    Final version with jar-file and Read_me-message

--- src/main/java/by/microbiology/genom/GenomParser.java
+++ src/main/java/by/microbiology/genom/GenomParser.java
@@ -6,91 +6,43 @@ import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class GenomParser {
-	private static Logger LOGGER = LoggerFactory.getLogger(GenomParser.class);
-	private static String location = "";
-	private static String protein = "";
-	private static StringBuilder sequence = new StringBuilder();
-	private static String locusTag = "";
+//	private static Logger LOGGER = LoggerFactory.getLogger(GenomParser.class);
 	private static String line;
-
-	public static void writeCSV() throws Exception {
-		FileWriter fileWriter = new FileWriter("microbiology.csv", true);
-		try (PrintWriter writer = new PrintWriter(fileWriter)) {
-
-			StringBuilder sb = new StringBuilder();
-			sb.append(sequence);
-			sb.append(';');
-			sb.append(locusTag + "  PROTEIN: " + protein + "  LOCATION: " + location);
-			sb.append('\n');
-
-			writer.write(sb.toString());
+	public String path;
+	
+	public GenomParser(String[] args) {
+		try {
+			readAndSet(args);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	public static void writeCSVHead() throws Exception {
-		FileWriter fileWriter = new FileWriter("microbiology.csv", true);
-		try (PrintWriter writer = new PrintWriter(fileWriter)) {
-
-			StringBuilder sb = new StringBuilder();
-			sb.append("sequence");
-			sb.append(';');
-			sb.append("locus_tag");
-			sb.append('\n');
-
-			writer.write(sb.toString());
-		}
-	}
-
-	public static void setSequence(String line) throws Exception {
-		sequence.append(line);
-	}
-
-	public static void setLocusTag(String line) {
-		if (line.contains("locus_tag")) {
-			locusTag = line.substring(line.indexOf("locus_tag=") + 10);
-			locusTag = locusTag.substring(0, locusTag.indexOf("]"));
-		}
-	}
-
-	public static void setProtein(String line) {
-		protein = line.substring(line.indexOf("protein=") + 8);
-		protein = protein.substring(0, protein.indexOf("] "));
-	}
-
-	public static void setLocation(String line) {
-		location = line.substring(line.indexOf("location=") + 9);
-		location = location.substring(0, location.indexOf("] "));
-	}
-
-	public static void main(String[] args) throws Exception {
+	public void readAndSet(String[] args) throws Exception {
 		boolean first = true;
-		writeCSVHead();
-		Scanner pathScanner = new Scanner(System.in);
-		System.out.print("Please insert the fna path: ");
-		String path = pathScanner.next();
+		CsvWriter csv = new CsvWriter();
 
-		try (Scanner sc = new Scanner(new File(path))) {
+		SetData dat = new SetData();
+
+		try (Scanner sc = new Scanner(new File(args[0]))) {
 			while (sc.hasNextLine()) {
 				line = sc.nextLine();
 				if (line.charAt(0) != '>') {
-					setSequence(line);
+					dat.setSequence(line);
 				} else {
 					if (!first) {
-						writeCSV();
+						csv.writeCSV(dat.getSequence(), dat.getLocusTag(), dat.getProtein(), dat.getLocation());
 					}
 					first = false;
-					sequence = new StringBuilder();
-					setLocusTag(line);
-					setProtein(line);
-					setLocation(line);
+					dat.resetSequence();
+					dat.setLocusTag(line);
+					dat.setProtein(line);
+					dat.setLocation(line);
 				}
 			}
-			setSequence(line);
-			writeCSV();
+			dat.setSequence(line);
+			csv.writeCSV(dat.getSequence(), dat.getLocusTag(), dat.getProtein(), dat.getLocation());
 		}
-		pathScanner.close();
-		System.out.println("Done!");
 	}
 }
